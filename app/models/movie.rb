36f6b194c7821278618cf0b0d75bf31dd0314c84
rubensrvsc <rubensspfc100@gmail.com>
2022-11-20 15:38:45 -0300

    model: add scope from select ids

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
     validates :title, uniqueness: true
     has_and_belongs_to_many :actors
     has_many :comments
+
+    scope :selecte_movies_attributes, -> { select(:id, :title) }
 end
