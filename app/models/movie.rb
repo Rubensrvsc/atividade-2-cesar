e63841aa96ced82f1bf80877b61e334fb61b2122
rubensrvsc <rubensspfc100@gmail.com>
2022-11-20 15:56:43 -0300

    feat: add validation from year movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,17 @@
 class Movie < ApplicationRecord
     validates :title, :year, presence: true
     validates :title, uniqueness: true
+    validate :validate_year 
     has_and_belongs_to_many :actors
     has_many :comments
 
     scope :selecte_movies_attributes, -> { select(:id, :title) }
+
+    private
+
+    def validate_year
+        if self.year.to_i < 0
+            self.errors.add(:year, "Should be more than zero")
+        end
+    end
 end
