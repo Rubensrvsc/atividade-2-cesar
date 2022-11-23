60ff169c51c4b415dc7fe92d1b42501182d44001
rubensrvsc <rubensspfc100@gmail.com>
2022-11-23 20:49:27 -0300

    test: rubocop files

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,32 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-    context '#success' do
-        before(:all) do
-            @actor = Actor.new(
-                name: 'actor 1',
-                email: 'otheractor@mail.com',
-                year: 1998
-            )
-        end
-
-        it { expect(@actor.valid?).to be_truthy }
-
-        it { expect(@actor.save).to be_truthy }
-        
+  context '#success' do
+    before(:all) do
+      @actor = Actor.new(
+        name: 'actor 1',
+        email: 'otheractor@mail.com',
+        year: 1998
+      )
     end
 
-    context '#failure' do
-        before(:all) do
-            @actor = Actor.new(
-                email: 'actor1@mail.com',
-                year: 1998
-            )
-        end
-        
-        it { expect(@actor.valid?).to be_falsey }
+    it { expect(@actor.valid?).to be_truthy }
 
-        it { expect(@actor.save).to be_falsey }
+    it { expect(@actor.save).to be_truthy }
+  end
 
+  context '#failure' do
+    before(:all) do
+      @actor = Actor.new(
+        email: 'actor1@mail.com',
+        year: 1998
+      )
     end
+
+    it { expect(@actor.valid?).to be_falsey }
+
+    it { expect(@actor.save).to be_falsey }
+  end
 end
